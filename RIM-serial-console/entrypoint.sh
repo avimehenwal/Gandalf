bd66066a2c9a057df9fa5308bca9d4f79688813d
avimehenwal <avi.mehanwal@gmail.com>
2020-04-07 17:03:42 +0200

    []

--- RIM-serial-console/entrypoint.sh
+++ RIM-serial-console/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Hurry! Entrypoint"
+echo "Hurrey! Entrypoint"
 # Forward connection from 8000 --> ttyUSB0 device
 # socat tcp-listen:8000,reuseaddr,fork \
 #     file:/dev/ttyUSB0,nonblock,waitlock=/var/run/tty0.lock,b115200,raw,echo=0
 
 # socat -d -d \
 #     TCP-LISTEN:8080,reuseaddr \
-#     EXEC:'cat client.http' \
-#     2>> http8080.log
+#     EXEC:'echo avi'
+
+# SERVER
+socat TCP-LISTEN:8080,reuseaddr pty
+# CLIENT
+# socat pty,link=/home/avi/ttyUSB0,waitslave TCP:localhost:8080
+
+# END
